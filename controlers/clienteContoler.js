c3836be97faedbbb598e959a90a8e013ac4b0e9e
Juan Jose Taboada Franco <juanjo.fra@gmail.com>
2020-04-23 20:08:29 -0400

    Desactivado el control del token

--- controlers/clienteContoler.js
+++ controlers/clienteContoler.js
@@ -1,78 +1,68 @@
-const Cliente = require('../models/Cliente');
-const { validationResult } = require('express-validator');
-
+const Cliente = require("../models/Cliente");
+const { validationResult } = require("express-validator");
 
 exports.index = async (req, res) => {
-    try {
-        const clientes = await cliente.find();
-        res.status(200).json({clientes});
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ msg: 'Error en la consulta'});
-    }
-   
-    
-}
+  try {
+    const clientes = await Cliente.find();
+    res.status(200).json({ clientes });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Error en la consulta" });
+  }
+};
 
 exports.store = async (req, res) => {
+  //Verificar si hay errores
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
 
-    //Verificar si hay errores
-    const errores = validationResult(req);
-    if(!errores.isEmpty()){
-        return res.status(400).json({errores: errores.array()});
-    }
+  //Creamos el cliente
+  let cliente = new Cliente(req.body);
 
-    //Creamos el cliente
-    let cliente = new Cliente(req.body);
-
-    await cliente.save();
-    res.status(200).json(cliente);
-
-}
-
-
+  await cliente.save();
+  res.status(200).json(cliente);
+};
 
 exports.update = async (req, res) => {
-    try {
+  try {
     const proyectoId = req.params.id;
     let proyecto = await Proyecto.findById(proyectoId);
-   
+
     //Verificar que sea el usuario logueado el que quira modificar el proyecto
-    if(proyecto.creador.toString() !== req.usuario.id){
-       return res.status(403).json({ msg: 'Permiso denegado lalala'});
+    if (proyecto.creador.toString() !== req.usuario.id) {
+      return res.status(403).json({ msg: "Permiso denegado lalala" });
     }
-   
+
     proyecto.nombre = req.body.nombre;
     await proyecto.save();
-    
-    res.status(201).json({ 
-        msg: 'Se actualizo el proyecto',
-        obj: proyecto
-    })
-    
 
-   } catch (error) {
+    res.status(201).json({
+      msg: "Se actualizo el proyecto",
+      obj: proyecto,
+    });
+  } catch (error) {
     console.log(error);
-       res.status(500).json({ msg: 'Error en el servidor'});
-   }
-}
+    res.status(500).json({ msg: "Error en el servidor" });
+  }
+};
 
-exports.eliminarProyecto = async (req, res) =>{
-    try {
-        const proyectoId = req.params.id;
-        let proyecto = await Proyecto.findById(proyectoId);
-       
-        //Verificar que sea el usuario logueado el que quira modificar el proyecto
-        if(proyecto.creador.toString() !== req.usuario.id){
-           return res.status(403).json({ msg: 'Permiso denegado lalala'});
-        }
+exports.eliminarProyecto = async (req, res) => {
+  try {
+    const proyectoId = req.params.id;
+    let proyecto = await Proyecto.findById(proyectoId);
 
-        await proyecto.remove();
-
-        res.status(201).json({ msg: 'Proyecto Eliminado'});
-    } catch (error) {
-        
-    console.log(error);
-    res.status(500).json({ msg: 'Error en el servidor'});
+    //Verificar que sea el usuario logueado el que quira modificar el proyecto
+    if (proyecto.creador.toString() !== req.usuario.id) {
+      return res.status(403).json({ msg: "Permiso denegado lalala" });
     }
-}
+
+    await proyecto.remove();
+
+    res.status(201).json({ msg: "Proyecto Eliminado" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Error en el servidor" });
+  }
+};
--- routes/clientes.js
+++ routes/clientes.js
@@ -1,24 +1,18 @@
 //Rutas de Clientes
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const clienteControler = require('../controlers/clienteContoler');
-const { check } = require('express-validator');
-const authMiddleware = require('../middleware/auth');
+const clienteControler = require("../controlers/clienteContoler");
+const { check } = require("express-validator");
+const authMiddleware = require("../middleware/auth");
 
 //Crear un Clientes
 //api/clientes
-router.get('/',
-    authMiddleware,
-    clienteControler.index
+router.get("/", clienteControler.index);
+
+router.post(
+  "/register",
+  [check("nombre", "El nombre es obligatorio").not().isEmpty()],
+  clienteControler.store
 );
-
-router.post('/', authMiddleware,
-    [
-        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
-    ],
-    clienteControler.store
-);
-
-
 
 module.exports = router;
